96f523134f3ca190422b079c27be70466682530f
Alex Beeken <aeb242@gmail.com>
2017-09-13 22:57:36 -0700

    test button for showing user data

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
+const { inject, computed } = Ember;
+const { service } = inject;
+
 export default Ember.Controller.extend({
+  user: null,
+  actions: {
+    getUserInfo() {
+      let user = this.store.queryRecord('user', { filter: { email: 'test@example.com' }})
+      this.set('user', user)
+    }
+  }
 });
