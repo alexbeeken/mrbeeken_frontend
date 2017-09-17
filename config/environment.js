4bb5d280e576fc8887baa1b78ceecf39873d1f2a
Alex Beeken <aeb242@gmail.com>
2017-09-17 08:52:48 -0700

    change api to be singular

--- config/environment.js
+++ config/environment.js
@@ -54,7 +54,7 @@ module.exports = function(environment) {
   ENV['ember-simple-auth-token'] = {
     refreshAccessTokens: true,
     refreshLeeway: 300, // Refresh the token 5 minutes (300s) before it expires.
-    serverTokenEndpoint: '/api/v1/sessions/login',
+    serverTokenEndpoint: '/api/v1/session/login',
     headers: {
       'Accept': 'application/vnd.api+json',
       'Content-Type': 'application/vnd.api+json'
