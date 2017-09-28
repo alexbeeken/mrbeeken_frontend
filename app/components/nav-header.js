5567ade74918dbf2f5edafb7c093837628fb5cae
Alex Beeken <aeb242@gmail.com>
2017-09-28 14:13:04 -0700

    rename component to nav-header

--- app/components/nav-header.js
+++ app/components/nav-header.js
@@ -1,14 +1,17 @@
 import Ember from 'ember';
 
 const { inject, computed } = Ember;
-const { service, controller } = inject;
+const { service } = inject;
 const { alias } = computed;
 
 export default Ember.Component.extend({
   session: service(),
   cuService: service('current-user'),
   currentUser: alias('cuService.user'),
-  loggedIn: computed('session.isAuthenticated', function() {
-    return this.get('session.isAuthenticated');
-  }),
+  loggedIn: alias('session.isAuthenticated'),
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
+  },
 });
