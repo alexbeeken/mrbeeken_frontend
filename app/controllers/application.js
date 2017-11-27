7da7bf7c4e3e9ac63cc59a993513eb8b8d7b3c5a
Alex Beeken <aeb242@gmail.com>
2017-11-26 20:43:10 -0800

    saves activities page when navigating away

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 const { inject, computed } = Ember;
-const { service } = inject;
+const { service, controller } = inject;
 const { alias } = computed;
 
 export default Ember.Controller.extend({
+  activitiesController: controller('activities'),
   actions: {
     transitionAway(newBlurb) {
+      let from = this.currentPath
+      if (from.includes('activities')) {
+        this.set('activitiesController.currentActivitiesSlide', from.replace('activities', ''))
+      }
       this.transitionToRoute(newBlurb)
     }
   }
