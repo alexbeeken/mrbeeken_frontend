9719cf5e341bd551653628f05040a4be733c996c
Alex Beeken <aeb242@gmail.com>
2017-11-27 16:47:37 -0800

    animate experience icons, make app kind of mobile friendly

--- app/components/experience-icon.js
+++ app/components/experience-icon.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
-const { inject, computed } = Ember;
-const { service } = inject;
-const { alias } = computed;
+const { computed } = Ember;
 
 export default Ember.Component.extend({
   path: null,
   isSvg: computed('path', function() {
     return /svg/.test(this.get('path'))
+  }),
+  animationClass: computed('path', function() {
+    let path = this.get('path')
+    if (this.get('isSvg')) {
+      return path.replace(/svg\//, "")
+    } else {
+      return path.replace(/png\//, "").replace(/.png/, "")
+    }
   })
 });
