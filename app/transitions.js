6f3beb55dcf17f9505eef60e9dd51d102f93c323
Alex Beeken <aeb242@gmail.com>
2017-12-08 17:54:27 -0800

    show/hide menu a little faster

--- app/transitions.js
+++ app/transitions.js
@@ -2,13 +2,7 @@ export default function(){
   this.transition(
     this.hasClass("show-menu"),
     this.toValue(true),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  )
-  this.transition(
-    this.hasClass("hide-menu"),
-    this.toValue(true),
-    this.use('crossFade'),
-    this.reverse('crossFade')
+    this.use('toRight', { duration: 250 }),
+    this.reverse('toLeft',  { duration: 250 })
   )
 }
