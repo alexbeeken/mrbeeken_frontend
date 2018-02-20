f9b3ff045b95262942bb7db2289966b72b89499d
Alex Beeken <aeb242@gmail.com>
2018-02-20 10:54:16 -0500

    fix route bug and remove about page

--- app/components/item-edit-link.js
+++ app/components/item-edit-link.js
@@ -4,6 +4,9 @@ const { computed } = Ember;
 const { alias } = computed;
 
 export default Ember.Component.extend({
+  itemEditRoute: computed('item', function() {
+    return this.get('itemName') + '.edit'
+  }),
   itemName: alias('item._internalModel.modelName'),
   item: null
 })
