b16278b59f66737590e0c34ae01f1484be303132
Alex Beeken <aeb242@gmail.com>
2018-02-25 21:52:09 -0800

    fix frustrating bug where enroll button showed at the wrong time

---
+++ app/routes/course/index.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+const { inject, computed } = Ember;
+const { service } = inject;
+const { alias } = computed;
+
+export default Ember.Route.extend({
+  cuService: service('current-user'),
+  currentUser: alias('cuService.user'),
+  setupController(controller, model) {
+    this._super(controller, model)
+    let showEnrollButton;
+    if (this.get('noCurrentUser')) {
+      showEnrollButton = false
+      controller.set('showEnrollButton', showEnrollButton)
+      return
+    }
+    this.get('store').query('course-enrollment', {
+      filter: {
+        user_id: this.get('currentUser.id'),
+        course_id: model.id
+      }
+    }).then(function(courseEnrollments) {
+      showEnrollButton = !(courseEnrollments.get('length') > 0)
+      controller.set('showEnrollButton', showEnrollButton)
+      return
+    }).catch(function() {
+      controller.set('showEnrollButton', true)
+    })
+  },
+});
